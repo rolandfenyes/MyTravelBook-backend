ce7fd5361a08bfa3f063a9722807d56962bd0c63
Roland Csaba Fényes <rolandcsabafenyes@edu.bme.hu>
2020-12-06 14:06:56 +0100

    mobile optimisation

--- ClientApp/src/app/person-costs-card/person-costs-card.component.ts
+++ ClientApp/src/app/person-costs-card/person-costs-card.component.ts
@@ -10,10 +10,18 @@ export class PersonCostsCardComponent implements OnInit {
 
   @Input()
   user!: User;
+  travels: number;
+  accommodations: number;
+  outgoings: number;
+  total: number;
   
   constructor() { }
 
   ngOnInit() {
+    this.travels = Math.round(this.user.travelCost * 100) / 100;
+    this.accommodations = Math.round(this.user.accommodationCost * 100) / 100;
+    this.outgoings = Math.round(this.user.outgoingsCost * 100) / 100;
+    this.total = Math.round(this.user.totalCost * 100) / 100;
   }
 
 }
